7e6d219c0fa153846d33e6309c95d8f8c57a1bf7
ZAHID ABDIKADIR <zahidabdikadir12@gmail.com>
2020-03-11 14:24:48 -0500

    fixed the problems

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
-$(document).ready(function() {
-  $("#js-shopping-list-form").submit(function(event) {
+"use strict";
+
+//Add Items - Submit Event Handler
+$(function() {
+  $("#js-shopping-list-form").submit(event => {
     event.preventDefault();
-    const newItem = $(".js-shopping-list-entry").val();
-    $(".js-shopping-list-entry").val("");
+    const newItem = $("#shopping-list-entry").val();
     $(".shopping-list").append(
-      `<li>
-        <span class="shopping-item">${newItem}</span>
-        <div class="shopping-item-controls">
-          <button class="shopping-item-toggle">
-            <span class="button-label">check</span>
-          </button>
-          <button class="shopping-item-delete">
-            <span class="button-label">delete</span>
-          </button>
-        </div>
-      </li>`
+      $(` <li>
+    <span class="shopping-item">${newItem}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`)
     );
   });
-  $(".shopping-list").on("click", ".shopping-item-delete", function(event) {
-    $(this)
-      .closest("li")
-      .remove();
-  });
+});
 
-  $(".shopping-list").on("click", ".shopping-item-toggle", function(event) {
-    $(this)
-      .closet("li")
-      .find(".shopping-item")
-      .toggleClass(".shopping-item_checked");
-  });
+// Check Functions - .closest() to parent element ('shopping-list)
+$(".shopping-list").on("click", ".shopping-item-toggle", function(event) {
+  $(this)
+    .closest("li")
+    .find(".shopping-item")
+    .toggleClass("shopping-item__checked");
+});
+
+// Delete/Remove Functions - use .toggleClass()
+$(".shopping-list").on("click", ".shopping-item-delete", function(event) {
+  $(this)
+    .closest("li")
+    .remove();
 });
